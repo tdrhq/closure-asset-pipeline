f2bafc513ccd9b313196f5110f2d85ae4e4406f3
Arnold Noronha <arnstein87@gmail.com>
2011-10-25 22:45:57 -0700

    convention over configuration here too. untested.

--- lib/closure-asset-pipeline/railtie.rb
+++ lib/closure-asset-pipeline/railtie.rb
@@ -1,14 +1,15 @@
 module ClosureProcessor
   class Railtie < Rails::Engine
     config.closure = ActiveSupport::OrderedOptions.new
-    config.closure.lib = 'vendor/assets/closure-library/closure'
+    config.closure.compress = false
 
     initializer :setup_closure do |app|
-      app.assets.append_path config.closure.lib
-      app.assets.append_path 'vendor/assets'
+        app.assets.append_path File.expand_path("../..", ClosureDependenciesProcessor.closure_library)
+        app.assets.register_engine '.soy', SoyTemplateProcessor
 
-      app.assets.register_preprocessor 'application/javascript', ClosureDependenciesProcessor
-      app.assets.register_engine '.soy', SoyTemplateProcessor
+      if config.closure.compress         
+        app.assets.register_preprocessor 'application/javascript', ClosureDependenciesProcessor
+      end
     end
 
   end
