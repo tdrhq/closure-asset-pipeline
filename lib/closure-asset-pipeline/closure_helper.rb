34cdd47f7adabfc50619019be462235b5e204632
Arnold Noronha <arnstein87@gmail.com>
2011-10-25 23:10:00 -0700

    just reopen ApplicationHelper, alright?

--- lib/closure-asset-pipeline/closure_helper.rb
+++ lib/closure-asset-pipeline/closure_helper.rb
@@ -1,5 +1,5 @@
 
-module ClosureHelper
+module ApplicationHelper
   def google_closure_prefix
     Rails.env.production? ? "<script src='/assets/javascripts/closure-library/closure/goog/base.js'></script>" : ""
   end
