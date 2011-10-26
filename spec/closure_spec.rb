0a0f3fbefdaf7c138594614e427d2b4a7ae3b177
Arnold Noronha <arnstein87@gmail.com>
2011-10-25 19:29:29 -0700

    first test.

--- spec/closure_spec.rb
+++ spec/closure_spec.rb
@@ -1,4 +1,5 @@
 require "./lib/closure-asset-pipeline/directive_processor"
+require "tempfile"
 
 describe 'closure-prepreprocessor' do
   it "should have a valid closure compiler" do
@@ -7,6 +8,16 @@ describe 'closure-prepreprocessor' do
 
   it "should have a valid closure library directory" do
     File.directory?(ClosureDependenciesProcessor.closure_library).should be_true()
+  end
+
+  it "should do a basic compilation" do
+    f1 = Tempfile.new("some")
+    f1.puts <<EOF
+alert("this is a test");
+EOF
+    f1.flush
+
+    ClosureDependenciesProcessor.new(f1.path).render.should match(/this is a test/)
   end
 end
 
