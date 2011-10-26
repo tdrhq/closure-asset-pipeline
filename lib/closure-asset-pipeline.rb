d0a2263297ea8a6384a04519999a72dafb96958e
Arnold Noronha <arnstein87@gmail.com>
2011-10-25 23:05:35 -0700

    ...

--- lib/closure-asset-pipeline.rb
+++ lib/closure-asset-pipeline.rb
@@ -3,4 +3,6 @@ require "tilt"
 require "closure-asset-pipeline/version"
 require "closure-asset-pipeline/directive_processor"
 require "closure-asset-pipeline/soy_processor"
+require "closure-asset-pipeline/closure_helper"
 require "closure-asset-pipeline/railtie" if defined?(Rails)
+
--- lib/closure-asset-pipeline/railtie.rb
+++ lib/closure-asset-pipeline/railtie.rb
@@ -4,9 +4,9 @@ module ClosureProcessor
     config.closure.compress = false
 
     initializer :setup_closure do |app|
-        app.assets.append_path File.expand_path("../..", ClosureDependenciesProcessor.closure_library)
-        app.assets.register_engine '.soy', SoyTemplateProcessor
-
+      app.assets.append_path File.expand_path("../..", ClosureDependenciesProcessor.closure_library)
+      app.assets.register_engine '.soy', SoyTemplateProcessor
+      
       if config.closure.compress         
         app.assets.register_preprocessor 'application/javascript', ClosureDependenciesProcessor
       end
