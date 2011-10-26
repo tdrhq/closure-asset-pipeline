2ef7318e5176a3cd947587209b3301c7e692609a
Arnold Noronha <arnstein87@gmail.com>
2011-10-25 19:22:14 -0700

    bleh.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "http://rubygems.org"
 
 # Specify your gem's dependencies in closure-sprockets.gemspec
 gemspec
+
+gem 'rspec'
--- lib/closure-asset-pipeline/directive_processor.rb
+++ lib/closure-asset-pipeline/directive_processor.rb
@@ -1,22 +1,17 @@
+require "tilt"
+
 class ClosureDependenciesProcessor < Tilt::Template
   def prepare; end
 
+  def self.closure_bin
+    File.expand_path("../../../vendor/assets/javascripts/closure-library/closure/bin/build/closurebuilder.py", __FILE__)
+  end
+
+  def self.closure_library
+    File.expand_path("../../../vendor/assets/javascripts/closure-library/", __FILE__)
+  end
+
   def evaluate(context, locals, &block)
-    context.require_asset 'goog/base'
-    context.require_asset 'soyutils'
-
-    data.lines.each do |line|
-
-      if line =~ /goog\.require\s*\(\s*[\'\"]([^\)]+)[\'\"]\s*\)/
-        goog, mod, sub = $1.split(".")
-        next if mod =~ /^Test/
-        sub = mod if sub.nil?
-
-        dep = [goog, mod, sub].compact.join("/").downcase
-        context.require_asset(dep)
-      end
-    end
-
     data
   end
 end
